ee4ac5cfcad10ece7a9ee26e8bd530214cc79b6c
Yeonjung Hong <hongyj1230@gmail.com>
2019-02-21 00:57:55 -0500

    font/menu/logo changes

--- unit_9/js/scripts.js
+++ unit_9/js/scripts.js
@@ -3,5 +3,6 @@ $( document ).ready(function() {
     console.log( "hello" );
     $( "#menu_button" ).click(function() {
   		$("nav").slideToggle();
+  		 $("#menu_button").toggleClass("active");
 	});
 });
